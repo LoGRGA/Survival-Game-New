29b35d90d3c81cec4b9e2d9ad7f4c60656dae315
LoGRGA <jerrylowjj@gmail.com>
2025-04-02 02:34:08 +0800

    First Commit - Part 2

---
+++ Assets/Shop/HaziqScripts/InventoryController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+
+    //will check if the slot if full or not
+    public bool[] isFull;
+    //slots for inventory
+    public GameObject[] slots;
+}
---
+++ Assets/Shop/HaziqScripts/OpenInventory.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenInventory : MonoBehaviour
+{
+
+    //reference to inventory system
+    public GameObject inventoryScreenUI;
+
+    //check if inventory screen is open or not
+    public bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && !isOpen)
+        {
+
+            Debug.Log("i is pressed");
+            inventoryScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            isOpen = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && isOpen)
+        {
+            inventoryScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            isOpen = false;
+        }
+    }
+}
