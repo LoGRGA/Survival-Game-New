5e1903071f6eadc46fddaaa2b758d414143e0792
yayuovo <2930788707@qq.com>
2025-04-04 16:41:42 +0800

    fix billboard script

--- Assets/Scripts/Enemies/HealthBar/BillBoard.cs
+++ Assets/Scripts/Enemies/HealthBar/BillBoard.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BillBoard : MonoBehaviour
 {
- //   private Transform cam;
+    private Transform cam;
+    //public Transform cam;
 
-   // void Start()
-    //{
-     //   cam = GameObject.FindGameObjectWithTag("Player").transform.Find("Main Camera");
-    //}
+    void Start()
+    {
+       cam = GameObject.FindGameObjectWithTag("Player").transform.Find("Body/Main Camera");
+    }
     // Update is called once per frame
-    //void LateUpdate()
-    //{
-      //  transform.LookAt(transform.position + cam.forward);
-    //}
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
 }
+
+
+//learn from https://www.youtube.com/watch?v=3JjBJfoWDCM
